f9a25eaf477434360d879e8f75a88f7181eac75c
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-01-05 22:23:28 -0800

    Updated demo

--- demo/index.js
+++ demo/index.js
@@ -51,11 +51,7 @@ class Demo extends React.Component {
           <InlineEdit
             maxRows={5}
             defaultValue="
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-          Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-          Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-          Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
-          In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+           Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
           "
           />
         </div>
